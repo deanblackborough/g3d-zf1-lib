405632a1b282253015d8d2aa46ca4e43084a9a55
Dean Blackborough <dean@g3d-development.com>
2015-07-10 16:44:40 +0100

    Bar
    1. Base code for progress bar view helper.
    2. Corrected a typo in the layout.

---
+++ library/G3d/View/BootstrapProgressBar.php
@@ -0,0 +1,63 @@
+<?php
+/**
+* Generates the HTML for the bootstrap progress bar compnent
+* 
+* @author Dean Blackborough <dean@g3d-development.com>
+* @copyright G3D Development Limited
+* @license https://github.com/deanblackborough/g3d-zf1-lib/blob/master/LICENSE.md
+*/
+class G3d_View_BootstrapProgressBar extends Zend_View_Helper_Abstract 
+{
+	/**
+	* Override the hinting to allow code completion for our view helpers
+	*
+	* @var G3d_View_Codehinting
+	*/
+	public $view;
+		
+	/**
+	* Set options
+	* 
+	* @return G3d_View_BootstrapBadge
+	*/
+	public function bootstrapProgressBar() 
+	{
+		$this->resetParams();
+		
+		return $this;
+	}
+	
+	/**
+	* Reset any internal params, interal properties need to be reset so that 
+	* if the view helper is called within the view script each request is 
+	* unique
+	* 
+	* @return void
+	*/
+	private function resetParams() 
+	{
+		
+	}
+	
+	/**
+	* Generate the HTML for the progress bar based on all defined options 
+	* 
+	* @return string 
+	*/
+	private function render() 
+	{
+		return '';
+	}
+	
+	/**
+	* Define __toString to allow the result to be returned when echo 
+	* and print are called on the object, simply calls the private render 
+	* method
+	* 
+	* @return string The generated html
+	*/
+	public function __toString() 
+	{
+		return $this->render();
+	}
+}
